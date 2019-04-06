241d87209f1ef27d1e3f33cb96636900192297e5
Wesley Fuchter <wesley.fuchter@gmail.com>
2019-04-05 20:52:52 -0400

    Implements user crud endpoints.

--- webapp/src/Tasks.js
+++ webapp/src/Tasks.js
@@ -1,9 +1,62 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import './Tasks.css';
 
 function Tasks() {
+
+	const [open, setOpen] = useState(false);
+
+	const handleClickOpen = () => {
+		setOpen(true);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	};
+
 	return (
-		<h1>Tasks</h1>
+		<div>
+			<Fab className={'fab-button'} color="primary" aria-label="Add" onClick={handleClickOpen}>
+				<AddIcon />
+			</Fab>
+			<Dialog
+				open={open}
+				onClose={handleClose}
+				aria-labelledby="form-dialog-title">
+				<DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+				<DialogContent>
+					<DialogContentText>
+						To subscribe to this website, please enter your email address here. We will send
+						updates occasionally.
+					</DialogContentText>
+					<TextField
+						autoFocus
+						margin="dense"
+						id="name"
+						label="Email Address"
+						type="email"
+						fullWidth
+					/>
+				</DialogContent>
+				<DialogActions>
+					<Button onClick={handleClose} color="primary">
+						Cancel
+					</Button>
+					<Button onClick={handleClose} color="primary">
+						Subscribe
+					</Button>
+				</DialogActions>
+			</Dialog>
+		</div>
 	);
 }
 
