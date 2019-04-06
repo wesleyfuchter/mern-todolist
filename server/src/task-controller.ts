7dba5ca0c5d2f7e70fc2e19be04a36fce07a9c10
Wesley Fuchter <wesley.fuchter@gmail.com>
2019-04-06 10:18:57 -0400

    Implements user crud endpoints.

--- server/src/task-controller.ts
+++ server/src/task-controller.ts
@@ -12,21 +12,8 @@ class TaskController implements Controller {
             return response.status(201).json(await Task.create(request.body));
         });
         router.put("/tasks/:id", async (request: Request, response: Response): Promise<Response> => {
-            return response.status(200).json(await Task.updateOne({_id: request.params.id}, request.body));
-        });
-        router.put("/tasks/:id/markAsDone", async (request: Request, response: Response): Promise<Response> => {
-            const task = await Task.findById(request.params.id);
-            if (task) {
-                return response.status(200).json(await Task.updateOne({_id: request.params.id}, {done: true, ...task}));
-            }
-            return response.status(404);
-        });
-        router.put("/tasks/:id/markAsTodo", async (request: Request, response: Response): Promise<Response> => {
-            const task = await Task.findById(request.params.id);
-            if (task) {
-                return response.status(200).json(await Task.updateOne({_id: request.params.id}, {done: false, ...task}));
-            }
-            return response.status(404);
+            await Task.updateOne({_id: request.params.id}, request.body);
+            return response.status(200).json(request.body);
         });
         router.delete("/tasks/:id", async (request: Request, response: Response): Promise<Response> => {
             return response.status(204).json(await Task.deleteOne({_id: request.params.id}));
--- webapp/src/Tasks.js
+++ webapp/src/Tasks.js
@@ -9,6 +9,9 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
 import './Tasks.css';
+import Card from "@material-ui/core/Card";
+import {Checkbox, Divider} from "@material-ui/core";
+import Icon from '@material-ui/core/Icon';
 
 function Tasks() {
 
@@ -29,6 +32,28 @@ function Tasks() {
 		const data = await submit();
 		setTasks([...tasks, data]);
 		setOpen(false);
+	};
+
+	const handleFindAll = async () => {
+		const data = await findAll();
+		setTasks([...tasks, ...data]);
+	};
+
+	const handleMarkAsDone = async (id) => {
+		const currentTask = tasks.find(task => task._id === id);
+		const data = await markAs(currentTask, true);
+		setTasks([...(tasks.filter(task => task._id !== id)), data]);
+	};
+
+	const handleMarkAsTodo = async (id) => {
+		const currentTask = tasks.find(task => task._id === id);
+		const data = await markAs(currentTask, false);
+		setTasks([...(tasks.filter(task => task._id !== id)), data]);
+	};
+
+	const handleDelete = async (taskToDelete) => {
+		setTasks([...(tasks.filter(task => task._id !== taskToDelete._id))]);
+		await deleteTask(taskToDelete);
 	};
 
 	const findAll = async () => {
@@ -64,18 +89,87 @@ function Tasks() {
 			.then(response => response.json());
 	};
 
+	const markAs = async (task, done) => {
+		const headers = new Headers({
+			"Content-Type": "application/json",
+		});
+		const request = {
+			method: 'PUT',
+			headers: headers,
+			mode: 'cors',
+			cache: 'default',
+			body: JSON.stringify({...task, done: done})
+		};
+		return await fetch(`http://localhost:3333/tasks/${task._id}`, request)
+			.then(response => response.json());
+	};
+
+	const deleteTask = async (task) => {
+		const headers = new Headers({
+			"Content-Type": "application/json",
+		});
+		const request = {
+			method: 'DELETE',
+			headers: headers,
+			mode: 'cors',
+			cache: 'default'
+		};
+		return await fetch(`http://localhost:3333/tasks/${task._id}`, request);
+	};
+
 	useEffect(() => {
-		findAll().then(data => setTasks([...tasks, ...data]));
+		handleFindAll();
 	}, []);
 
 	return (
 		<div>
-			<ul>
-				{(tasks.map(task => <li key={task._id}>{task.title}</li>))}
-			</ul>
+
+			{tasks.filter(task => !task.done).length === 0 && <h1 className={`youre-done`}>You're done!</h1>}
+
+			{tasks.filter(task => !task.done).length > 0 &&
+			(<ul>
+				{(tasks.filter(task => !task.done).map(task => {
+					return (
+						<Card className={'item-list-card'} key={task._id}>
+							<Checkbox
+								checked={task.done}
+								onChange={({target}) => handleMarkAsDone(target.value)}
+								value={task._id}
+							/>
+							{task.title}
+							<Button className={'delete-button'} onClick={() => handleDelete(task)}>
+								<Icon>delete</Icon>
+							</Button>
+						</Card>
+					);
+				}))}
+			</ul>)}
+
+			{tasks.filter(task => task.done).length > 0 && <Divider/>}
+
+			{tasks.filter(task => task.done).length > 0 &&
+			(<ul>
+				{(tasks.filter(task => task.done).map(task => {
+					return (
+						<Card className={'item-list-card'} key={task._id}>
+							<Checkbox
+								checked={task.done}
+								onChange={({target}) => handleMarkAsTodo(target.value)}
+								value={task._id}
+							/>
+							{task.title}
+							<Button className={'delete-button'} onClick={() => handleDelete(task)}>
+								<Icon>delete</Icon>
+							</Button>
+						</Card>
+					);
+				}))}
+			</ul>)}
+
 			<Fab className={'fab-button'} color="primary" aria-label="Add" onClick={handleClickOpen}>
 				<AddIcon/>
 			</Fab>
+
 			<Dialog
 				open={open}
 				onClose={handleCancel}
@@ -104,6 +198,7 @@ function Tasks() {
 					</Button>
 				</DialogActions>
 			</Dialog>
+
 		</div>
 	);
 }
